15215e4e7396567f0b0172a0cddfa6bcddbdc0a7
Margui <margui.berjon@gmail.com>
2020-04-23 18:06:06 +0200

    revert changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,7 @@ function Form(props) {
   return (
     <form className='form' onSubmit={props.onSubmit}>
       <input className='fom-input-text' type='search' onChange={props.onNameChange} placeholder={props.placeholderSearch}
-        value={localStorage.getItem('userSearch')}>
+      >
       </input>
     </form >
   );
