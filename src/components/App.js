c4464969f18a4a42cd554a722e74e4e9a5c69413
Margui <margui.berjon@gmail.com>
2020-04-23 10:13:39 +0200

    Search by name working

--- src/components/App.js
+++ src/components/App.js
@@ -5,33 +5,37 @@ import Header from './Header';
 import Form from './Form';
 import List from './List'
 
+let allCharacters = [];
+let filters = { name: '' };
 
 function App() {
   const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     getCharactersFromAPI().then(characters => {
+      allCharacters = characters;
       setCharacters(characters)
     })
   }, []);
 
   function onNameChange({ target }) {
     console.log(target);
-    const userSearch = target.value;
-    setCharacters(applyFilters())
+    filters.name = target.value;
+    setCharacters(applyFilters());
   }
 
   function applyFilters() {
-    //Si filters.name.length es true (hay algo escrito) ? se hace el filtro de allcharacter por nombre, si es false (no hay nada escrito) : se muestran todos los characters. 
-    filters.name.length ? characters.filter(elem => elem.name.toUpperCase().includes(filters.name.toUpperCase())) : characters;
-
-    return (
-      <>
-        <Header></Header>
-        <Form placeholderSearch='Busca tu personaje' onNameChange={onNameChange}></Form>
-        <List list={characters}></List>
-      </>
-    );
+    //Si filters.name.length es true (hay algo escrito) ? se hace el filtro de allcharacter por nombre, si es false (no hay nada escrito) : se muestran todos los characters.
+    return filters.name.length ? allCharacters.filter(elem => elem.name.toUpperCase().includes(filters.name.toUpperCase())) : allCharacters;
   }
 
-  export default App;
+  return (
+    <>
+      <Header></Header>
+      <Form placeholderSearch='Busca tu personaje' onNameChange={onNameChange}></Form>
+      <List list={characters}></List>
+    </>
+  );
+}
+
+export default App;
