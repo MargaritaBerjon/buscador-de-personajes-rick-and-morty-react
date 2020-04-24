ab8b9f3a4f2407393c69f810bb7dd122446d00c5
Margui <margui.berjon@gmail.com>
2020-04-24 18:57:30 +0200

    Update docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.323b6165f97e2647048b111f1fdb504b.js"
+  "./precache-manifest.47ba361bdc9ce48b81077cf6adc2665e.js"
 );
 
 self.addEventListener('message', (event) => {
