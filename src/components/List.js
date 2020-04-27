7579affc66a9f5bb305b45950c135407cf8e0971
Margui <margui.berjon@gmail.com>
2020-04-27 11:22:57 +0200

    sort

--- src/components/List.js
+++ src/components/List.js
@@ -8,7 +8,7 @@ function List(props) {
 
   return (
     < main className="list" >
-      {props.list.length ? props.list.sort((elem, otherelem) => elem.name.localeCompare(otherelem.name)) && props.list.map((elem, i) => <Card key={i} elem={elem}></Card>) : <div className='list-no-results'>
+      {props.list.length ? props.list.sort((elem, otherelem) => elem.name.localeCompare(otherelem.name)).map((elem, i) => <Card key={i} elem={elem}></Card>) : <div className='list-no-results'>
         <h2 className='list-no-content'>No hay ningún personaje que coincida con la búsqueda "{props.value}"</h2>
         <img className='no-content-img' src={noresults} alt='Rick and Morty'></img>
       </div>}
